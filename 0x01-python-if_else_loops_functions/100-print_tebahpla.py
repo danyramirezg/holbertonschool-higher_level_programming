44d75a2f2afd8d1d05e1bcf1ebb77504cbe0b219
Dany <danyramirezg@gmail.com>
2019-09-11 11:51:42 -0500

    Remove one print function and fix style

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for alp in range(122, 96, -1):
     if alp % 2 == 0:
-        print('{:c}'.format(alp), end='')
+        alp = alp
     else:
-        print('{:c}'.format(alp - 32), end='')
-
+        alp = alp - 32
+    print('{:c}'.format(alp), end='')
