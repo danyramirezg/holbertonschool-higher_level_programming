fdee5e755e583d230a13b10b631d7df4cef116a0
Dany <danyramirezg@gmail.com>
2019-09-12 10:51:44 -0500

    Fix pep8 style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__=="__main__":
+if __name__ == "__main__":
     from sys import argv
     argc = len(argv) - 1
     if argc < 2:
@@ -8,5 +8,5 @@ if __name__=="__main__":
         print("{} argument: ".format(argc))
     else:
         print("{} arguments:".format(argc))
-    for i in range(1, argc +1):
+    for i in range(1, argc + 1):
         print("{}: {}".format(i, argv[i]))
