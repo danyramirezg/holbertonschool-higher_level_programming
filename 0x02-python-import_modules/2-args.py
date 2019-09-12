28cc9d7e743e0aa1ba5818e0729609cfa81c8829
Dany <danyramirezg@gmail.com>
2019-09-12 14:49:36 -0500

    Revove space

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     argc = len(argv) - 1
     if argc == 1:
-        print("{} argument: ".format(argc))
+        print("{} argument:".format(argc))
     elif argc == 0:
         print("{} arguments.".format(argc))
     else:
