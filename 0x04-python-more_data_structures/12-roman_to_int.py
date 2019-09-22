5d232ab4b4533c49298f99bdad8fe45f251d3a86
Dany <danyramirezg@gmail.com>
2019-09-22 08:58:32 -0500

    function def roman_to_int(roman_string): that converts a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
-        return 0
     dicc_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                 'M': 1000}
-    num = 0
-    cont = 0
-    for i in roman_string:
+    rom = 0
+    result = 0
+    parameter = roman_string
+    if type(parameter) is not str or len(parameter) is 0:
+        return 0
+    for i in parameter:
         if i in dicc_rom:
             value = dicc_rom[i]
         else:
-            raise ValueError("roman_string contains illegal character: " + i)
+            raise ValueError("roman_string contains illegal character: " + parameter)
 
-        if num is 0:
-            num += value
-            cont += 1
+        if rom is 0:
+            rom += value
+            result += 1
             continue
         if i == 'L' or i == 'C':
-            if roman_string[cont - 1] == 'X':
+            if parameter[result - 1] == 'X':
                 value -= 20
         elif i == 'D' or i == 'M':
-            if roman_string[cont - 1] == 'C':
+            if parameter[result - 1] == 'C':
                 value -= 200
         elif i == 'V' or i == 'X':
-            if roman_string[cont - 1] == 'I':
+            if parameter[result - 1] == 'I':
                 value -= 2
-        num += value
-        cont += 1
-    return num
+        rom += value
+        result += 1
+    return rom
