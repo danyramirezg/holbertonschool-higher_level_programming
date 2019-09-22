c70fe61bc5342b53b8f794a9e04ff47134f80ef6
Dany <danyramirezg@gmail.com>
2019-09-22 08:27:28 -0500

    Fix pep8 style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
     dicc_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                 'M': 1000}
+                'M': 1000}
     num = 0
     cont = 0
     for i in roman_string:
