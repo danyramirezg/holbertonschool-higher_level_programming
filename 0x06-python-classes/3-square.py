1ee741a85efeadb0b25d48df73ec7e48c2d91c6a
Dany <danyramirezg@gmail.com>
2019-10-01 17:48:40 -0500

    Public instance method: def area(self): that returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Creates a Square class"""
+
+    def __init__(self, size=0):
+        """Initializes Square"""
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+
+        area = self.__size * self.__size
+
+        return area
