92dd84263651c1268f83de417b49a38d162a137d
Dany <danyramirezg@gmail.com>
2019-10-01 19:32:35 -0500

    Private instance attribute: size: property def size(self): to retrieve it property setter def size(self, value): to set it

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Creates a Square class"""
+
+    def __init__(self, size=0):
+        """Initializes Square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+
+        area = self.__size * self.__size
+
+        return area
