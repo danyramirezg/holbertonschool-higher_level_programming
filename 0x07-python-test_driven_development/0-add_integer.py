1c99bafe778ba2d34c860d2968df9a0a9e8f3d3e
dany <danyramirezg@gmail.com>
2019-10-04 15:41:22 -0500

    Add more test and add comments in the function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Function that adds 2 integers
+a and b must be integers or floats, otherwise raise a TypeError exception with the
+message a must be an integer or b must be an integer
 
-"""Function that adds 2 integers"""
+"""
 
 
 def add_integer(a, b=98):
-
+    """
+     a and b must be first casted to integers if they are float
+    """
     if type(a) is int or type(a) is float:
         a = int(a)
     else:
