51cdc6df8fe88f7bce9d3b5a67a09469e43ffd20
Dany <danyramirezg@gmail.com>
2019-10-04 22:07:22 -0500

    Fix pep8 style

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Function that adds 2 integers
-a and b must be integers or floats, otherwise raise a TypeError exception with the
-message a must be an integer or b must be an integer
+a and b must be integers or floats,
+otherwise raise a TypeError
 
 """
 
