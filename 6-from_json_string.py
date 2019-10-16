86c06f36d8b1971be33142b03f48a59f484c82fc
Dany <danyramirezg@gmail.com>
2019-10-16 18:59:01 -0500

    function that returns an object (Python data structure) represented by a JSON string

---
+++ 6-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""function that returns an object (Python data structure)
+ represented by a JSON string"""
+
+def from_json_string(my_str):
+    with open(my_str, encoding="UTF8") as file:
+
+
+
