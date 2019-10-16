91c0c027acb120edadb04739127c9c283ec073ce
Dany <danyramirezg@gmail.com>
2019-10-16 11:56:46 -0500

    Create the mains functions

---
+++ 0x0B-python-input_output/2-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+read_lines = __import__('2-read_lines').read_lines
+
+print("1 line:")
+read_lines("my_file_0.txt", 1)
+print("--")
+print("3 lines:")
+read_lines("my_file_0.txt", 3)
+print("--")
+print("Full content:")
+read_lines("my_file_0.txt")
+
+guillaume@ubuntu:~/0x0B$ cat my_file_0.txt
+Holberton School offers a truly innovative approach to education:
+focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers.
+
+A school every software engineer would have dreamt of!
