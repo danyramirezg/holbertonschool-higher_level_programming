81bb31ad8c422907db3f3141826ab481155c164a
Dany <danyramirezg@gmail.com>
2019-10-20 23:05:17 -0500

    Update the class Rectangle by adding the public method def update(self, *args, **kwargs)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,7 +94,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                        self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
 
         if args:
             if len(args) >= 1:
@@ -107,3 +107,7 @@ class Rectangle(Base):
                 self.x = args[3]
             if len(args) >= 5:
                 self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                #if hassattr(self, key) is True:
+                    setattr(self, key, value)
