00271faea3a3dbd6541ea1324b2df6cb56d94c97
dany <danyramirezg@gmail.com>
2019-10-22 17:33:26 -0500

    Fix output

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,7 +47,7 @@ class Base:
         """static method that returns the list of the JSON string
         representation"""
         if json_string is None:
-            return "[]"
+            return []
         return json.loads(json_string)
 
     @classmethod
