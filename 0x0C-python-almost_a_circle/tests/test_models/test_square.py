b4aaf3801b5d4362ca0311a59b91e481b5aee479
Dany <danyramirezg@gmail.com>
2019-10-22 23:24:33 -0500

    More tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -13,89 +13,89 @@ class SquareTest(unittest.TestCase):
 
     def test_is_base(self):
         self.assertIsInstance(Square(10), Base)
-
-    def test_is_rectangle(self):
-        self.assertIsInstance(Square(10), Square)
-
-    def test_no_args(self):
-        with self.assertRaises(TypeError):
-            Square()
-
-    def test_one_arg(self):
-        s1 = Square(10)
-        s2 = Square(11)
-        self.assertEqual(s1.id, s2.id - 1)
-
-    def test_two_args(self):
-        s1 = Square(10, 2)
-        s2 = Square(2, 10)
-        self.assertEqual(s1.id, s2.id - 1)
-
-    def test_three_args(self):
-        s1 = Square(10, 2, 2)
-        s2 = Square(2, 2, 10)
-        self.assertEqual(s1.id, s2.id - 1)
-
-    def test_four_args(self):
-        self.assertEqual(7, Square(10, 2, 2, 7).id)
-
-    def test_more_than_four_args(self):
-        with self.assertRaises(TypeError):
-            Square(1, 2, 3, 4, 5)
-
-    def test_size_private(self):
-        with self.assertRaises(AttributeError):
-            print(Square(10, 2, 3, 4).__size)
-
-    def test_size_getter(self):
-        self.assertEqual(5, Square(5, 2, 3, 9).size)
-
-    def test_size_setter(self):
-        s = Square(4, 1, 9, 2)
-        s.size = 8
-        self.assertEqual(8, s.size)
-
-    def test_width_getter(self):
-        s = Square(4, 1, 9, 2)
-        s.size = 8
-        self.assertEqual(8, s.width)
-
-    def test_height_getter(self):
-        s = Square(4, 1, 9, 2)
-        s.size = 8
-        self.assertEqual(8, s.height)
-
-    def test_x_getter(self):
-        self.assertEqual(0, Square(10).x)
-
-    def test_y_getter(self):
-        self.assertEqual(0, Square(10).y)
-
-
-# class TestSquare_size(unittest.TestCase):
-#     """Unittests for testing size initialization of the Square class."""
 #
-#     def test_None_size(self):
-#         with self.assertRaisesRegex(TypeError, "width must be an integer"):
-#             Square(None)
+#     def test_is_rectangle(self):
+#         self.assertIsInstance(Square(10), Square)
 #
-#     def test_str_size(self):
-#         with self.assertRaisesRegex(TypeError, "width must be an integer"):
-#             Square("invalid")
+#     def test_no_args(self):
+#         with self.assertRaises(TypeError):
+#             Square()
 #
-#     def test_float_size(self):
-#         with self.assertRaisesRegex(TypeError, "width must be an integer"):
-#             Square(5.5)
+#     def test_one_arg(self):
+#         s1 = Square(10)
+#         s2 = Square(11)
+#         self.assertEqual(s1.id, s2.id - 1)
 #
-#     def test_complex_size(self):
-#         with self.assertRaisesRegex(TypeError, "width must be an integer"):
-#             Square(complex(5))
+#     def test_two_args(self):
+#         s1 = Square(10, 2)
+#         s2 = Square(2, 10)
+#         self.assertEqual(s1.id, s2.id - 1)
 #
-#     def test_dict_size(self):
-#         with self.assertRaisesRegex(TypeError, "width must be an integer"):
-#             Square({"a": 1, "b": 2}, 2)
+#     def test_three_args(self):
+#         s1 = Square(10, 2, 2)
+#         s2 = Square(2, 2, 10)
+#         self.assertEqual(s1.id, s2.id - 1)
 #
-#     def test_bool_size(self):
+#     def test_four_args(self):
+#         self.assertEqual(7, Square(10, 2, 2, 7).id)
+#
+#     def test_more_than_four_args(self):
+#         with self.assertRaises(TypeError):
+#             Square(1, 2, 3, 4, 5)
+#
+#     def test_size_private(self):
+#         with self.assertRaises(AttributeError):
+#             print(Square(10, 2, 3, 4).__size)
+#
+#     def test_size_getter(self):
+#         self.assertEqual(5, Square(5, 2, 3, 9).size)
+#
+#     def test_size_setter(self):
+#         s = Square(4, 1, 9, 2)
+#         s.size = 8
+#         self.assertEqual(8, s.size)
+#
+#     def test_width_getter(self):
+#         s = Square(4, 1, 9, 2)
+#         s.size = 8
+#         self.assertEqual(8, s.width)
+#
+#     def test_height_getter(self):
+#         s = Square(4, 1, 9, 2)
+#         s.size = 8
+#         self.assertEqual(8, s.height)
+#
+#     def test_x_getter(self):
+#         self.assertEqual(0, Square(10).x)
+#
+#     def test_y_getter(self):
+#         self.assertEqual(0, Square(10).y)
+#
+#
+# # class TestSquare_size(unittest.TestCase):
+# #     """Unittests for testing size initialization of the Square class."""
+# #
+# #     def test_None_size(self):
+# #         with self.assertRaisesRegex(TypeError, "width must be an integer"):
+# #             Square(None)
+# #
+# #     def test_str_size(self):
+# #         with self.assertRaisesRegex(TypeError, "width must be an integer"):
+# #             Square("invalid")
+# #
+# #     def test_float_size(self):
+# #         with self.assertRaisesRegex(TypeError, "width must be an integer"):
+# #             Square(5.5)
+# #
+# #     def test_complex_size(self):
+# #         with self.assertRaisesRegex(TypeError, "width must be an integer"):
+# #             Square(complex(5))
+# #
+# #     def test_dict_size(self):
+# #         with self.assertRaisesRegex(TypeError, "width must be an integer"):
+# #             Square({"a": 1, "b": 2}, 2)
+# #
+# #     def test_bool_size(self):
 #         with self.assertRaisesRegex(TypeError, "width must be an integer"):
 #             Square(True, 2, 3)
 #
