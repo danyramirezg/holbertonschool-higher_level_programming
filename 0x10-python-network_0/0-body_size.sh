e474cde867d170488055f0dec72ee59e45966f0c
danyramirezg <danyramirezg@gmail.com>
2020-01-06 20:57:34 +0000

    takes in a URL, sends a request to that URL, and displays the size of the body of the response. The size must be displayed in bytes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
-# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# The size must be displayed in bytes
-
+#!/bin/bash
+# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response. The size must be displayed in bytes
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f2 
