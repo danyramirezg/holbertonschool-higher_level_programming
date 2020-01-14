62e77717519c07acc856255a2a53cb383a45c9c2
Dany <danyramirezg@gmail.com>
2020-01-13 23:51:53 -0500

    Task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id"""
+
+import requests
+from sys import argv
+
