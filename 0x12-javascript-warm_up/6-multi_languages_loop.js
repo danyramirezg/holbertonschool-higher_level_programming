9c100853318e9a61e1e2722963b46bc2d9eca4f8
Dany <danyramirezg@gmail.com>
2020-02-04 16:59:03 -0500

    script that prints 3 lines, using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Script that prints 3 lines */
+
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i;
+
+for (i = 0; i < lang.length; i++) { console.log(lang[i]); }
