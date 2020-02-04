ddcb3f8893c3858ba7b16a6454777de3d6d88c76
Dany <danyramirezg@gmail.com>
2020-02-04 17:56:43 -0500

    script that prints a square”

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* Script that prints a square */
+
+const square = 'x';
+const argum = process.argv[2];
+let i;
+
+if (isNaN(argum) || process.argv.length === 2) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < argum; i++) {
+    console.log(square.repeat(argum));
+  }
+}
