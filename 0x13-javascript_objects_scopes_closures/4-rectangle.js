439087dc46c6330b84d7ca1b7f49cdf187e8d542
Dany <danyramirezg@gmail.com>
2020-02-10 17:19:09 -0500

    Method called rotate() that exchanges the width and the height of the rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+// Class Rectangle. Create an instance method called rotate() that exchanges the width and the height of the rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let rows = 0; rows < this.height; rows++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
