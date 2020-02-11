5c5fab1b6444f874f01208174002581a98b11330
Dany <danyramirezg@gmail.com>
2020-02-11 13:47:56 -0500

    Reads and prints the content of a file. If an error occurred during the reading, print the error object. Fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script that reads and prints the content of a file
 
-var fs = require('fs');
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
