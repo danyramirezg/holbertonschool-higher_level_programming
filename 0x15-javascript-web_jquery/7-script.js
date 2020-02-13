3961b196467c5e839b214c27b9467beb978cefb1
dany <danyramirezg@gmail.com>
2020-02-13 18:56:51 -0500

    fetches and replaces the name of this URL: https://swapi.co/api/people/5/?format=json

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+// Fetches and replaces the name of this URL: https://swapi.co/api/people/5/?format=json
+
+const url = 'https://swapi.co/api/people/5/?format=json';
+$.get(url, function (data) {
+  $('DIV#character').text(data.name);
+});
